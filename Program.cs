4c8a6ae4ecbf40b7c0f51a531412347de4281b3a
Berkay Demir (1000123364) <DEMIB1@student.op.ac.nz>
2023-09-12 11:18:22 +1200

    nice

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@
             Console.WriteLine(@"wassup gang");
 
             Console.WriteLine("whaaaaaaaaa00");
+            int num;
+            Console.WriteLine("ready for take off");
+            for(int i = 1; i <= 10; i++)
+            {
+               num = i;
+                Console.WriteLine(num);
+            }
 
         }
     }
