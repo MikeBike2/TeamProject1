b49307a088875ab383a733a1da59ab3b9dd987bb
Elijah Hiraka (1000104429) <HIRAE1@student.op.ac.nz>
2023-09-12 11:17:45 +1200

    WHOS SMELLY

--- Program.cs
+++ Program.cs
@@ -4,13 +4,20 @@
     {
         static void Main(string[] args)
         {
-
+            string whosSmelly = "Not me";
             Console.WriteLine("Hello, World, my name is Elijah");
 
             Console.WriteLine(@"wassup gang");
 
             Console.WriteLine("whaaaaaaaaa00");
+            bool smells = false;
 
+            while (smells == false)
+            {
+                Console.Write("Smelly ");
+                smells = true;
+            }
+            Console.WriteLine($"Smelly? {whosSmelly}");
         }
     }
 }
